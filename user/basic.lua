9b2fb0debfc3ded7441407ecb890c09a6315d64a
venjiang <venjiang@gmail.com>
2023-02-06 21:17:18 +0800

    clean

--- user/basic.lua
+++ user/basic.lua
@@ -2,3 +2,5 @@ vim.o.timeout = true
 vim.o.timeoutlen = 300
 lvim.colorscheme = "onedarker"
 lvim.leader = ","
+lvim.builtin.alpha.active = false
+lvim.builtin.nvimtree.setup.view.side = "right"
--- user/plugins.lua
+++ user/plugins.lua
@@ -1,6 +1,23 @@
 vim.g.copilot_assume_mapped = true
 -- vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+local function telescope_find_files(_)
+	require("lvim.core.nvimtree").start_telescope("find_files")
+end
+
+local function telescope_live_grep(_)
+	require("lvim.core.nvimtree").start_telescope("live_grep")
+end
+lvim.builtin.nvimtree.setup.view.mappings.list = {
+	{ key = "u", action = "dir_up" },
+
+	{ key = { "l", "<CR>", "o" }, action = "edit", mode = "n" },
+	{ key = "h", action = "close_node" },
+	{ key = "v", action = "vsplit" },
+	{ key = "C", action = "cd" },
+	{ key = "gtf", action = "telescope_find_files", action_cb = telescope_find_files },
+	{ key = "gtg", action = "telescope_live_grep", action_cb = telescope_live_grep },
+}
 
 lvim.plugins = {
 	-- onedark
@@ -15,20 +32,7 @@ lvim.plugins = {
 			require("leap").set_default_keymaps()
 		end,
 	},
-	-- {
-	--   "ggandor/flit.nvim",
-	--   config = function()
-	--     require('flit').setup({
-	--       keys = { f = 'f', F = 'F', t = 't', T = 'T' },
-	--       -- A string like "nv", "nvo", "o", etc.
-	--       labeled_modes = "v",
-	--       multiline = true,
-	--       -- Like `leap`s similar argument (call-specific overrides).
-	--       -- E.g.: opts = { equivalence_classes = {} }
-	--       opts = {}
-	--     })
-	--   end
-	-- },
+
 	-- git
 	{
 		"tpope/vim-fugitive",
@@ -60,33 +64,8 @@ lvim.plugins = {
 			require("octo").setup()
 		end,
 	},
-	-- {
-	-- 	"phaazon/hop.nvim",
-	-- 	branch = "v2", -- optional but strongly recommended
-	-- 	config = function()
-	-- 		-- you can configure Hop the way you like here; see :h hop-config
-	-- 		-- require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-	-- 		require("hop").setup()
-	-- 	end,
-	-- },
 	-- copilot
 	"github/copilot.vim",
-	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	cmd = "Copilot",
-	-- 	event = "InsertEnter",
-	-- 	config = function()
-	-- 		require("copilot").setup({})
-	-- 	end,
-	-- },
-	-- gopher
-	-- {
-	-- 	"olexsmir/gopher.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 	},
-	-- },
 	"leoluz/nvim-dap-go",
 	"ray-x/go.nvim",
 	{ "ray-x/guihua.lua", build = "cd lua/fzy && make" },
@@ -107,63 +86,3 @@ formatters.setup({
 
 require("guihua.maps").setup()
 require("go").setup()
--- local gopher = require("gopher")
--- gopher.setup({
--- 	commands = {
--- 		go = "go",
--- 		gomodifytags = "gomodifytags",
--- 		gotests = "gotests",
--- 		impl = "impl",
--- 		iferr = "iferr",
--- 	},
--- })
-
--- local lsp_manager = require("lvim.lsp.manager")
--- lsp_manager.setup("golangci_lint_ls", {
--- 	on_init = require("lvim.lsp").common_on_init,
--- 	capabilities = require("lvim.lsp").common_capabilities(),
--- })
-
--- go lsp
--- local dap_ok, dapgo = pcall(require, "dap-go")
--- if not dap_ok then
--- 	return
--- end
-
--- dapgo.setup()
--- lsp_manager.setup("gopls", {
--- 	on_attach = function(client, bufnr)
--- 		require("lvim.lsp").common_on_attach(client, bufnr)
--- 		local _, _ = pcall(vim.lsp.codelens.refresh)
--- 		local map = function(mode, lhs, rhs, desc)
--- 			if desc then
--- 				desc = desc
--- 			end
-
--- 			vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
--- 		end
--- 		map("n", "<leader>Ci", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
--- 		map("n", "<leader>Ct", "<cmd>GoMod tidy<cr>", "Tidy")
--- 		map("n", "<leader>Ca", "<cmd>GoTestAdd<Cr>", "Add Test")
--- 		map("n", "<leader>CA", "<cmd>GoTestsAll<Cr>", "Add All Tests")
--- 		map("n", "<leader>Ce", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
--- 		map("n", "<leader>Cg", "<cmd>GoGenerate<Cr>", "Go Generate")
--- 		map("n", "<leader>Cf", "<cmd>GoGenerate %<Cr>", "Go Generate File")
--- 		map("n", "<leader>Cc", "<cmd>GoCmt<Cr>", "Generate Comment")
--- 		map("n", "<leader>DT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
--- 	end,
--- 	on_init = require("lvim.lsp").common_on_init,
--- 	capabilities = require("lvim.lsp").common_capabilities(),
--- 	settings = {
--- 		gopls = {
--- 			usePlaceholders = true,
--- 			gofumpt = true,
--- 			codelenses = {
--- 				generate = false,
--- 				gc_details = true,
--- 				test = true,
--- 				tidy = true,
--- 			},
--- 		},
--- 	},
--- })
