e0f99793fcc06a45f2154e5ddb85fac1344a6cc0
venjiang <venjiang@gmail.com>
2023-02-09 02:26:34 +0800

    line wrap

--- user/basic.lua
+++ user/basic.lua
@@ -1,7 +1,11 @@
 vim.o.timeout = true
 vim.o.timeoutlen = 300
+vim.opt.wrap = true
 lvim.colorscheme = "onedarker"
 lvim.leader = ","
 lvim.builtin.alpha.active = false
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.terminal.open_mapping = "<C-t>"
+-- fg = "#abb2bf",
+-- bg = "#1f2227",
+vim.cmd("au ColorScheme * hi VertSplit ctermfg=8 ctermbg=none guifg=#292C33 guibg=none")
